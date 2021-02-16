6a5361fd720a258df84f8cb6799aaa04130ded53
Jason <68907860+JJ-REDRAGON@users.noreply.github.com>
2021-02-16 15:22:55 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,321 @@
+function quiz(){
+	alert("Pearl crew assistant quiz")
+	alert("answer 23 questions to be an assistant")
+	alert("Lets begin boi")
+var q1= prompt("1. which was first? Mario or Sonic?");
+var score=0;
+lc=q1.toLowerCase();
+if (lc=="mario") {
+
+	alert("Correct anwser");
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q2= prompt("2. what is the most famous game from 2019 to 2020?");
+lc=q2.toLowerCase();
+if (lc=="minecraft") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q3= prompt("3. what was the first video game?");
+lc=q3.toLowerCase();
+if (lc=="pong") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+	alert("wrong answer")
+}
+
+var q4= prompt("4. what was the most famous Emote of Fortnite?");
+lc=q4.toLowerCase();
+if (lc=="floss") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q5= prompt("5. what was the rumor on Minecraft in 2010?");
+lc=q5.toLowerCase();
+if (lc=="Herobrine") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q6= prompt("6. what is the famous server in Minecraft?");
+lc=q6.toLowerCase();
+if (lc=="Hypixel") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q7= prompt("7. who is the smartest minecraft player");
+lc=q7.toLowerCase();
+if (lc=="Dream") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q8= prompt("8. what video game was made into a cartoon at 2003?");
+lc=q8.toLowerCase();
+if (lc=="Sonic the hedghog") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q9= prompt("9. what video game was ban in pakistan, but came back?");
+lc=q9.toLowerCase();
+if (lc=="PubG") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q10= prompt("10. what Game was Ban in the UAE?");
+lc=q10.toLowerCase();
+if (lc=="Roblox") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q11= prompt("11. What was the year of the release date of Fortnite?");
+lc=q11.toLowerCase();
+if (lc=="2017") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q12= prompt("12. which year was smg added to fortnite?");
+lc=q12.toLowerCase();
+if (lc=="2018") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q13= prompt("13. who were the creators of Mario?");
+lc=q13.toLowerCase();
+if (lc=="Nintendo") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q14= prompt("14. Which year was the legendary and pump shotgun added to fortnite?");
+lc=q14.toLowerCase();
+if (lc=="2018") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q15= prompt("15. who were the creators of Sonic the hedghog?");
+lc=q15.toLowerCase();
+if (lc=="Sega") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q16= prompt("16. What Was the theme of Chapter 1 Season 8 of fortnite?");
+lc=q16.toLowerCase();
+if (lc=="Pirates") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q17= prompt("17. What is the rarest glider in fotnite?");
+lc=q17.toLowerCase();
+if (lc=="Rainbow Rider") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q18= prompt("18. Who were the creators of Minecraft?");
+lc=q18.toLowerCase();
+if (lc=="Mojang") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q19= prompt("19. Who were the creators of Fortnite?");
+lc=q19.toLowerCase();
+if (lc=="Epic Games") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q20= prompt("20. Who were the creators of PubG?");
+lc=q20.toLowerCase();
+if (lc=="PUBG corporation") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q21= prompt("21. Who were the creators of Free Fire?");
+lc=q21.toLowerCase();
+if (lc=="Garena") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q22= prompt("22. What are the competitors of Free Fire?");
+lc=q22.toLowerCase();
+if (lc=="PubG") {
+
+	alert("Correct answer")
+	score++;
+}
+
+else{
+
+	alert("wrong answer")
+}
+
+var q23= prompt("23. What is your favourite Gaming company? lol");
+lc=q23.toLowerCase();
+if (lc=="Mojang") {
+
+	alert("Cool!")
+	score++;
+}
+
+else{
+
+	alert("Cool")
+}
+
+var q23= prompt("24. 2+2?");
+lc=q23.toLowerCase();
+if (lc=="5") {
+
+	alert("Good job my guy")
+	score++;
+}
+
+else{
+
+	alert("lol what, wrong")
+}
+
+
+
+alert("Thank you for playing! Your score is="+score)
+alert("coded by GOODRAGON")
+}
